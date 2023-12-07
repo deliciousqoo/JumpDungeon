618f481b913fd696fec823b371e03b952b6b54aa
신지한 <deliciousqoo@naver.com>
2023-12-07 18:20:36 +0900

    통합프로젝트 정리

--- Jumpmap/Assets/Scripts/CameraManager.cs
+++ Jumpmap/Assets/Scripts/CameraManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+public class TempCameraManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject camera;
