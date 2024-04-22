d9a52236d18093e7642fdeb59876d789e327cbab
신지한 <deliciousqoo@naver.com>
2024-04-22 22:36:41 +0900

    파이어베이스 베이스 작업
    파이어베이스 베이스 작업
    파이어베이스 회원가입, 로그인, 로그아웃 기능 구현

---
+++ Jumpmap/Assets/Scripts/Firebase/FirebaseManager.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using TMPro;
+using System;
+
+public class FirebaseManager
+{
+    public static FirebaseManager instance = null;
+    
+    public static FirebaseManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new FirebaseManager();
+            }
+
+            return instance;
+        }
+    }
+
+    private FirebaseAuth auth; // 로그인, 회원가입 등에 사용
+    private FirebaseUser user; // 인증이 완료된 유저 정보
+
+    public string UserId => user.UserId;
+
+    public Action<bool> LoginState;
+
+    public void Init()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+        if (auth.CurrentUser != null)
+        {
+            LogOut();
+        }
+        auth.StateChanged += OnChanged;
+    }
+
+    private void OnChanged(object sender, EventArgs e)
+    {
+        if (auth.CurrentUser != user)
+        {
+            bool signed = (auth.CurrentUser != user && auth.CurrentUser != null);
+            if (!signed && user != null)
+            {
+                Debug.Log("로그아웃");
+                LoginState?.Invoke(false);
+            }
+
+            user = auth.CurrentUser;
+            if (signed)
+            {
+                Debug.Log("로그인");
+                LoginState?.Invoke(true);
+            }
+        }
+    }
+
+    public void Create(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => 
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("회원가입 취소");
+                return;
+            }
+            if(task.IsFaulted)
+            {
+                Debug.Log("회원가입 실패");
+                return;
+            }
+
+            AuthResult authResult = task.Result;
+            FirebaseUser newUser = authResult.User;
+            Debug.Log("회원가입 성공");
+        });
+    }
+
+    public void Login(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.Log("로그인 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.Log("로그인 실패");
+                return;
+            }
+
+            AuthResult authResult = task.Result;
+            FirebaseUser newUser = authResult.User;
+            Debug.Log("로그인 성공");
+        });
+    }
+
+    public void LogOut()
+    {
+        auth.SignOut();
+        Debug.Log("로그아웃");
+    }
+}
